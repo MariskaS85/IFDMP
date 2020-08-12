e49a9d441f279922d7e34e6fa98d58f6bd133ff5
MariskaS85 <mcfsnijdewind@gmail.com>
2020-08-12 14:31:41 +0000

    Second try

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+
+//Nav functions
+
+function openNav() {
+    document.getElementById("myNav").style.width = "50%";
+            }
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+// Functions on Click!
+
+
+function foundHermit(){
+document.getElementById("myBtn").innerHTML = "Found Him!";
+}
+
+function foundMessage() {
+    setTimeout(function(){ alert("Are you ready for the next challenge?"); }, 2000);
+}
+
+
+
+//var btn = document.getElementById("myBtn");
+
+// btn.addEventListener("click", foundHermit);
+// btn.addEventListener("click", foundMessage);
+
+*/
